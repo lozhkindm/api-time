6b22d16a19d6cac7ec87d556610159da9c5f7cbd
d.lozhkin <d.lozhkin@geekbrains.ru>
2021-09-27 16:44:01 +0300

    Iteration 3

--- handlers.go
+++ handlers.go
@@ -4,18 +4,45 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	tz := r.URL.Query().Get("tz")
-
-	if tz == "" {
-		tz = UTC
-	}
-
 	w.Header().Add("Content-Type", "application/json")
 
+	tz := getTimezone(r)
+	tzs := strings.Split(tz, ",")
+
+	if len(tzs) > 1 {
+		multipleTimezoneHandler(w, tzs)
+	} else {
+		singleTimezoneHandler(w, tz)
+	}
+}
+
+func multipleTimezoneHandler(w http.ResponseWriter, tzs []string) {
+	tzmap := make(map[string]time.Time)
+
+	for _, tz := range tzs {
+		loc, err := time.LoadLocation(tz)
+
+		if err != nil {
+			http.Error(w, "invalid timezone", http.StatusNotFound)
+			return
+		}
+
+		tzmap[tz] = time.Now().In(loc)
+	}
+
+	err := json.NewEncoder(w).Encode(tzmap)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func singleTimezoneHandler(w http.ResponseWriter, tz string) {
 	loc, err := time.LoadLocation(tz)
 
 	if err != nil {
@@ -32,4 +59,14 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getTimezone(r *http.Request) string {
+	tz := r.URL.Query().Get("tz")
+
+	if tz == "" {
+		tz = UTC
+	}
+
+	return tz
 }
